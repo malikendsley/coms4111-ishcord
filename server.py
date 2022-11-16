6b1fc75bf97a0b2a3622d3421a520d69270ab3db
Malik Endsley (R15 Laptop) <malik.s.endsley@gmail.com>
2022-11-16 02:49:17 -0500

    Fix  user registration, start to use preferences

--- server.py
+++ server.py
@@ -68,6 +68,22 @@ def authd(request):
 def server_selected(request):
 	return request.cookies.get('server') is not None and request.cookies.get('channel') is not None
 
+def retrieve_prefs(uid):
+    	# retrieve preferences from db
+	try:
+		cursor = g.conn.execute("""
+			select color as user_color, text_size, primary_color, accent_color, line_spacing 
+   			from users, theme_profiles_creates where theme_profiles_creates.uid = %s 
+      		and theme_profiles_creates.name = current_theme;
+			""", uid)
+		prefs = cursor.fetchone()
+		columns = cursor.keys()
+		cursor.close()
+	except:
+		if cursor:
+			cursor.close()
+		return None
+	return dict(zip(columns, prefs))
 # ============== These functions serve pages ================== #
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -84,39 +100,40 @@ def register():
 	elif request.method == 'POST':
 		print("User registration request")
 		name = request.form['username']
-		color = request.form['color'][1:]
-		print("Color: ", color)
-		# check if user exists
-		cursor = g.conn.execute("SELECT name FROM users WHERE name = %s", name)
+		color = request.form['color'][1:]		
+	try: 
+		# require unique, non-empty username
 		if len(name) == 0:
-			flash("Username cannot be empty")
-			cursor.close()
-			return redirect('/register')
-		elif cursor.rowcount > 0:
-			flash('User already exists')
-			cursor.close()
-			return redirect('/register')
-		# create new user, then go to dashboard
-		else:
-			cursor.close()
-			# try to create user in users table
-			try:
-				cursor = g.conn.execute("INSERT INTO users(name, color) VALUES (%s, %s)", name, color)
-				# get uid of new user
-				cursor = g.conn.execute("SELECT uid FROM users WHERE name = %s", name)
-				uid = cursor.fetchone()[0]
-				print(type(uid))
-				print("New user created:", name, uid)
-				resp = make_response(redirect('/dashboard'))
-				resp.set_cookie('name', name)
-				resp.set_cookie('uid', str(uid))
-				return resp
-			except Exception as e:
-				print("Error creating user:", e)
-				flash("Error creating user, please try again")
-				return redirect('/register')
-			finally:
-				cursor.close()
+			raise Exception("Please enter a username")
+		cursor = g.conn.execute("SELECT * FROM users WHERE name = %s", name)
+		if cursor.rowcount > 0:
+			raise Exception("Username already taken")
+		cursor.close()
+		# create new user and default theme, redirect to dashboard
+		cursor = g.conn.execute("""
+            BEGIN;
+            INSERT INTO users (name, color) VALUES (%s, %s);
+            INSERT INTO theme_profiles_creates (name, uid) VALUES ('default', currval('uid_sequence'));
+			UPDATE users SET current_theme = 'default' WHERE uid = currval('uid_sequence');
+   			COMMIT;
+            """, name, color)
+		# get uid of new user by name
+		cursor = g.conn.execute("SELECT uid FROM users WHERE name = %s", name)
+		uid = cursor.fetchone()[0]
+		
+		cursor.close()
+
+	except Exception as e:
+		print(f"Registration error: {e}")
+		flash(f"Registration error: {str(e)}")
+		return redirect(url_for('register'))
+  
+	print(f"User {name} registered with uid {uid} successfully")
+	resp = make_response(redirect(url_for('dashboard')))
+	resp.set_cookie('uid', str(uid))
+	resp.set_cookie('name', name)
+	flash(f"Welcome, {name}!")
+	return resp
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -157,21 +174,31 @@ def login():
 			return resp
 
 @app.route('/dashboard', methods=['GET'], defaults={'server': None, 'channel': None})
+@app.route('/dashboard/<server>/', methods=['GET'], defaults={'channel': None})
 @app.route('/dashboard/<server>/<channel>', methods=['GET'])
 @login_required
 def dashboard(server, channel):
+	prefs = retrieve_prefs(request.cookies.get('uid'))
+	if not prefs:
+		flash("Error retrieving preferences")
+	
 	print("User visited dashboard")
+	print(f"Server: {server}, Channel: {channel}")
 	# if server and channel are specified, set cookies
-	if server and channel:
-		resp = make_response(render_template('dashboard.html', name=request.cookies.get('name'), server=server, channel=channel, uid=request.cookies.get('uid')))
+	if server and not channel:
+		resp = make_response(render_template('dashboard.html', name=request.cookies.get('name'), server=server, channel="general", uid=request.cookies.get('uid'), prefs=prefs))
 		resp.set_cookie('server', server)
-		# strip quotes from channel name
+		resp.set_cookie('channel', "general")
+		return resp
+	if server and channel:
+		resp = make_response(render_template('dashboard.html', name=request.cookies.get('name'), server=server, channel=channel, uid=request.cookies.get('uid'), prefs=prefs))
+		resp.set_cookie('server', server)
 		resp.set_cookie('channel', str(channel))
 		return resp
 	# if not, just show dashboard
 	else:
 		print("No server or channel specified")
-		return render_template('dashboard.html', name=request.cookies.get('name'), server=server, channel=channel, uid=request.cookies.get('uid'))
+		return render_template('dashboard.html', name=request.cookies.get('name'), server=server, channel=channel, uid=request.cookies.get('uid'), prefs=prefs)
 
 @app.route('/', methods=['GET'])
 def index():
@@ -191,6 +218,18 @@ def logout():
 	resp.set_cookie('server', '', expires=0)
 	resp.set_cookie('channel', '', expires=0)
 	return resp
+
+@app.route('/profile', methods=['GET'])
+@login_required	
+def profile():
+	print("User visited profile page")
+
+	prefs = retrieve_prefs(request.cookies.get('uid'))
+	if not prefs:
+		flash("Error retrieving user preferences")
+		return redirect(url_for('dashboard'))
+	print(f"User preferences: {prefs}")
+	return render_template('profile.html', name=request.cookies.get('name'), uid=request.cookies.get('uid'), prefs=prefs)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -224,80 +263,70 @@ def post(server, channel):
 @app.route('/api/<uid>/create_server', methods=['POST'])
 def create_server(uid):
 	# get details
-	name = request.form['server-name']
-	description = request.form['server-description']
+	server_name = request.form['server-name']
+	server_desc = request.form['server-description']
 
-	print("User created server:", name, description, uid)
+	print(f"User {uid} try create server:", server_name, server_desc)
 	# insert into db
 	try:
 		# check if server exists
-		cursor = g.conn.execute("SELECT name FROM forums_administrates WHERE name = %s", name)
+		cursor = g.conn.execute("SELECT name FROM forums_administrates WHERE name = %s", server_name)
 		if cursor.rowcount > 0:
 			cursor.close()
-			print("Server already exists")
-			flash("Server already exists")
-			return redirect('/dashboard')
-		# create server
+			raise Exception("Server already exists")
+		# try to setup server
 		else:
 			cursor.close()
 			# TOOD: implement image URI
-			g.conn.execute("INSERT INTO forums_administrates (name, description, uid) VALUES (%s, %s, %s)", name, description, uid)
-			print("New server created:", name, description, uid)
-			# get fid of new server 
-			cursor = g.conn.execute("SELECT fid FROM forums_administrates WHERE name = %s", name)
-			fid = cursor.fetchone()[0]
-			cursor.close()
+			nextfid = g.connn.execute("select nextval('forums_administrates_fid_seq')").fetchone()[0]
+			if nextfid is None:
+				raise Exception("Error getting next fid")
+			g.conn.execute("""
+				BEGIN;
+    			INSERT INTO forums_administrates (name, description, uid) VALUES (%s, %s, %s);
+				INSERT INTO channels_contains (cname, topic, fid) VALUES ('general', "general chat goes here", %s);
+            	INSERT INTO member_of (uid, fid) VALUES (%s, %s);
+				COMMIT;
+              	""", 
+          		server_name, server_desc, uid, nextfid, uid, nextfid)
+			print(f"Server named {server_name} created, general channel created, user {uid} added to server")
 	except Exception as e:
 		print("Error creating server:", e)
-		flash("Error creating server, please try again")
-		redirect('/dashboard')
- 	# create default general channel
-	try:
-		g.conn.execute("INSERT INTO channels_contains (cname, topic, fid) VALUES (%s, %s, %s)", "general", "general chat goes here", fid)
-		print("New channel created:", "general", name)
-	except Exception as e:
-		print("Error creating channel:", e)
-		flash("Error creating channel, please try again")
-		redirect('/dashboard')
-	# add creator to server
-	try:
-		# add creator to server
-		g.conn.execute("INSERT INTO member_of (uid, fid) VALUES (%s, %s)", uid, fid)
-		print("User added to server:", uid, fid)
-	except Exception as e:
-		print("Error adding user to server")
-		flash("Error adding user to server")
-		redirect('/dashboard')
+		flash(f"Error creating server: {str(e)}")
+		return redirect('/dashboard')
+
 	flash("Server created successfully")
 	return redirect('/dashboard')
 
+# create a new channel
 @app.route('/api/<fid>/create_channel', methods=['POST'])
 def create_channel(fid):
 	# get details
 	name = request.form['channel-name']
 	topic = request.form['channel-topic']
 
-	print("User created channel:", name, topic, fid)
-	# insert into db
+	print(f"New channel {name} created in server {fid}")
 	try:
 		# check if channel exists
 		cursor = g.conn.execute("SELECT cname FROM channels_contains WHERE cname = %s AND fid = %s", name, fid)
 		if cursor.rowcount > 0:
 			cursor.close()
-			print("Channel already exists")
-			flash("Channel already exists")
-			return redirect('/dashboard')
+			raise Exception("Channel already exists")
 		# create channel
 		else:
 			cursor.close()
 			g.conn.execute("INSERT INTO channels_contains (cname, topic, fid) VALUES (%s, %s, %s)", name, topic, fid)
-			print("New channel created:", name, topic, fid)
+			print("New channel created")
+		# get cname of new channel
+		cursor = g.conn.execute("SELECT cname FROM channels_contains WHERE cname = %s AND fid = %s", name, fid)
+		cname = cursor.fetchone()[0]
 	except Exception as e:
-		print("Error creating channel:", e)
-		flash("Error creating channel, please try again")
-		redirect('/dashboard')
+		print(f"Error creating channel: {e}")
+		flash(f"Error creating channel: {str(e)}")
+		return redirect(f'/dashboard/{fid}')
+
 	flash("Channel created successfully")
-	return redirect('/dashboard')
+	return redirect(f'/dashboard/{fid}/{cname}')
 
 # get all messages in a channel, long polling style
 @app.route('/api/<server>/<channel>/', methods=['GET'], defaults={'last': None})
